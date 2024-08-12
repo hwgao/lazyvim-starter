6e6785defd0575cf4b5d7388f3eb77193497acd5
Hongwei Gao <hongwei.gao@bytedance.com>
2024-08-12 11:01:28 +0800

    Remove twilight and zen-mode

--- lua/plugins/mine.lua
+++ lua/plugins/mine.lua
@@ -20,27 +20,6 @@ return {
   },
 
   {
-    "folke/twilight.nvim",
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
-
-  {
-    "folke/zen-mode.nvim",
-    keys = {
-      { "<leader>z", "<cmd>ZenMode<cr>", desc = "Toggle zen mode" },
-    },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
-
-  {
     "christoomey/vim-tmux-navigator",
     cmd = {
       "TmuxNavigateLeft",
