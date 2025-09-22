22ca7b9c2a5963ed18676ca4292d0e6bf8b11fb6
Hongwei Gao <hongwei.gao@vama.com>
2025-09-22 08:48:15 +0800

    add vscode.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.g.vscode then
+  require("config.vscode")
   -- Return or skip loading LazyVim related configurations
   return
 end
---
+++ lua/config/vscode.lua
@@ -0,0 +1,1 @@
+vim.keymap.set("n", "<Esc><Esc>", ":nohlsearch<CR>", { silent = true })
