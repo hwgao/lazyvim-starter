605ceb4e3a06244492b043bf9bd3d30b2e2d6723
Hongwei Gao <hongwei.gao@vama.com>
2025-09-28 10:20:16 +0800

    add keymaps to which-key

--- lua/plugins/vim-bookmarks.lua
+++ lua/plugins/vim-bookmarks.lua
@@ -1,13 +1,43 @@
 return {
   "MattesGroeger/vim-bookmarks",
-  keys = {
-    { "@t", "<Plug>BookmarkToggle", mode = "n", desc = "Bookmarks: Toggle" },
-    { "@a", "<Plug>BookmarkShowAll", mode = "n", desc = "Bookmarks: Show all" },
-    { "@c", "<Plug>BookmarkClear", mode = "n", desc = "Bookmarks: Clear all in current file" },
-    { "@C", "<Plug>BookmarkClearAll", mode = "n", desc = "Bookmarks: Clear all" },
-  },
   opts = {
     bookmark_no_default_key_mappings = 1,
   },
-  config = function() end,
+  config = function()
+    require("which-key").add({
+      { "<leader>M", group = "bookmarks" },
+      {
+        "<leader>MM",
+        function()
+          vim.cmd.BookmarkToggle()
+        end,
+        desc = "Toggle bookmark",
+        mode = "n",
+      },
+      {
+        "<leader>Ma",
+        function()
+          vim.cmd.BookmarkShowAll()
+        end,
+        desc = "Show all bookmarks",
+        mode = "n",
+      },
+      {
+        "<leader>Mc",
+        function()
+          vim.cmd.BookmarkClear()
+        end,
+        desc = "Clear all bookmarks in current file",
+        mode = "n",
+      },
+      {
+        "<leader>MC",
+        function()
+          vim.cmd.BookmarkClearAll()
+        end,
+        desc = "Clear all bookmarks",
+        mode = "n",
+      },
+    })
+  end,
 }
--- lua/plugins/vim-grepper.lua
+++ lua/plugins/vim-grepper.lua
@@ -3,19 +3,74 @@ return {
   opts = {
     highlight = 1,
   },
-  keys = {
-
-    { "@g", "<Plug>(GrepperOperator)", mode = { "n", "x" }, desc = "Grapper: Operator" },
-    {
-      "@w",
-      "<cmd>Grepper -tool rg -buffer -noprompt -highlight -cword<cr><cr>",
-      desc = "Grepper: Word under cursor in current file",
-    },
-    {
-      "@o",
-      "<cmd>Grepper -tool rg -buffers -noprompt -highlight -cword<cr><cr>",
-      desc = "Grepper: Word under cursor in open files",
-    },
-  },
-  config = function() end,
+  config = function()
+    require("which-key").add({
+      { "<leader>G", group = "grapper" },
+      {
+        "<leader>GG",
+        function()
+          vim.cmd.Grepper({ args = { "-tool", "rg", "-highlight" } })
+        end,
+        desc = "Search in root directory",
+        mode = "n",
+      },
+      {
+        "<leader>Gg",
+        function()
+          vim.cmd.Grepper({ args = { "-tool", "git", "-highlight" } })
+        end,
+        desc = "Search in the repository",
+        mode = "n",
+      },
+      {
+        "<leader>Gt",
+        function()
+          vim.cmd.Grepper({ args = { "-tool", "git", "-noprompt", "-query", "-E", "'(TODO|FIXME|XXX):'" } })
+        end,
+        desc = "Search todo in the repository",
+        mode = "n",
+      },
+      {
+        "<leader>G/",
+        function()
+          vim.cmd.Grepper({ args = { "-tool", "rg", "-noprompt", "-highlight", "-cword" } })
+        end,
+        desc = "Search word under cursor in root directory",
+        mode = "n",
+      },
+      {
+        "<leader>Gb",
+        function()
+          vim.cmd.Grepper({ args = { "-tool", "rg", "-buffer", "-noprompt", "-highlight", "-cword" } })
+        end,
+        desc = "Search word under cursor in current file",
+        mode = "n",
+      },
+      {
+        "<leader>GB",
+        function()
+          vim.cmd.Grepper({ args = { "-tool", "rg", "-buffers", "-noprompt", "-highlight", "-cword" } })
+        end,
+        desc = "Search word under cursor in open files",
+        mode = "n",
+      },
+    })
+  end,
 }
+
+--   keys = {
+--
+--     { "@g", "<Plug>(GrepperOperator)", mode = { "n", "x" }, desc = "Grapper: Operator" },
+--     {
+--       "@w",
+--       "<cmd>Grepper -tool rg -buffer -noprompt -highlight -cword<cr><cr>",
+--       desc = "Grepper: Word under cursor in current file",
+--     },
+--     {
+--       "@o",
+--       "<cmd>Grepper -tool rg -buffers -noprompt -highlight -cword<cr><cr>",
+--       desc = "Grepper: Word under cursor in open files",
+--     },
+--   },
+--   config = function() end,
+-- }
