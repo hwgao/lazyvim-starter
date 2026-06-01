af393d07bf72aadd1a75d255523cab55150475bc
Hongwei Gao <hhww.gao@gmail.com>
2026-06-01 10:08:08 +0800

    disable some plugins

--- lua/plugins/copilotchat.lua
+++ lua/plugins/copilotchat.lua
@@ -2,7 +2,7 @@ return {
   "CopilotC-Nvim/CopilotChat.nvim",
   opts = function(_, opts)
     -- Set default model to gpt-5.4
-    opts.model = "gpt-5.4-mini"
+    opts.model = "gpt-5.4"
 
     -- Optional: If you want to use "thinking" mode for better reasoning
     -- opts.reasoning_effort = "high"
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,4 @@
+return {
+  { "coder/claudecode.nvim", enabled = false },
+  { "NickvanDyke/opencode.nvim", enabled = false },
+}
