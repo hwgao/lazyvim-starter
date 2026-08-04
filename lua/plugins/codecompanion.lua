db684aec3596c55f1a7d7967aa1e2bbc09bbf971
Hongwei Gao <hhww.gao@gmail.com>
2026-08-04 18:21:13 +0800

    add keymaps to CodeCompanion

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,6 +1,13 @@
 return {
   "olimorris/codecompanion.nvim",
   version = "^19.0.0",
+  keys = {
+    { "<leader>C", nil, desc = "AI" },
+    { "<leader>CI", "<cmd>CodeCompanion<cr>", desc = "CodeCompanion Inline" },
+    { "<leader>CC", "<cmd>CodeCompanionChat<cr>", desc = "CodeCompanion Chat" },
+    { "<leader>CL", "<cmd>CodeCompanionCLI<cr>", desc = "Open CLI interaction" },
+    { "<leader>CA", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion Actions" },
+  },
   opts = {
     interactions = {
       cli = {
