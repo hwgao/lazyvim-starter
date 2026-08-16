df1ec8f731be8f72bf6b71a2760b3da58ec32c6a
Hongwei Gao <hhww.gao@gmail.com>
2026-08-16 14:39:56 +0800

    float terminal

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -11,10 +11,18 @@ return {
       desc = "CodeCompanion",
       mode = { "n", "v" },
     },
-    { "<leader>CI", "<cmd>CodeCompanion<cr>", desc = "CodeCompanion Inline", mode = { "n", "v" } },
-    { "<leader>CC", "<cmd>CodeCompanionChat<cr>", desc = "CodeCompanion Chat", mode = { "n", "v" } },
-    { "<leader>CL", "<cmd>CodeCompanionCLI<cr>", desc = "Open CLI interaction", mode = { "n", "v" } },
-    { "<leader>CA", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion Actions", mode = { "n", "v" } },
+    { "<leader>Ci", "<cmd>CodeCompanion<cr>", desc = "CodeCompanion Inline", mode = { "n", "v" } },
+    { "<leader>Cc", "<cmd>CodeCompanionChat<cr>", desc = "CodeCompanion Chat", mode = { "n", "v" } },
+    { "<leader>Cl", "<cmd>CodeCompanionCLI<cr>", desc = "Open CLI interaction", mode = { "n", "v" } },
+    { "<leader>Ca", "<cmd>CodeCompanionActions<cr>", desc = "CodeCompanion Actions", mode = { "n", "v" } },
+    {
+      "<leader>Ce",
+      "<cmd>CodeCompanion /explain<cr>",
+      desc = "Explain how selected code in a buffer works",
+      mode = { "v" },
+    },
+    { "<leader>Cf", "<cmd>CodeCompanion /fix<cr>", desc = "Fix the selected code", mode = { "v" } },
+    { "<leader>Cp", "<cmd>CodeCompanion /lsp<cr>", desc = "Explain the LSP diagnostics", mode = { "v" } },
   },
   opts = {
     interactions = {
