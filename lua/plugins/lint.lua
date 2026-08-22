a0eba0161869898faac06ef155213b3fb76905e3
Hongwei Gao <hhww.gao@gmail.com>
2026-08-22 17:16:03 +0800

    add markdownlint-cli2 configure

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -1,10 +1,10 @@
 return {
   "mfussenegger/nvim-lint",
-  opts = function(_, opts)
-    opts.linters = opts.linters or {}
-    opts.linters.markdownlint = {
-      args = { "--config", vim.fn.expand("~/.markdownlint-cli2.yaml"), "--" },
-    }
-    return opts
-  end,
+  opts = {
+    linters = {
+      ["markdownlint-cli2"] = {
+        prepend_args = { "--config", vim.fn.expand("~/.config/nvim/.markdownlint-cli2.yaml") },
+      },
+    },
+  },
 }
